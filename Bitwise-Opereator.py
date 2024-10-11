bdabb256602d5630692dc1f209c1ed859fe6a3e8
Zain2012-byte <muzaffartawseefzain@gmail.com>
2024-10-11 20:48:12 +0530

    Lesson.7

---
+++ Bitwise-Opereator.py
@@ -0,0 +1,24 @@
+a = 10   # Binary: 1010
+
+result = a << 2
+print(result)  # Output: 40 (Binary: 101000)
+
+a = 10   # Binary: 1010
+
+result = a >> 2
+print(result)  # Output: 2 (Binary: 0010)
+
+a=10
+b=-10
+
+#print bitwise right shiftoperator
+
+print("a >> 1 =", a>>1)
+print("b >> 1 =", b>>1)
+
+a=5
+
+b=-10
+
+print("a<<1=",a<<1)
+print("b<<1=",b<<1)
